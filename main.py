b4ed8d6164aceb131a401ae70aa5103e98e4362f
JosueLessie <joslessie@hotmail.com>
2023-10-10 21:18:17 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Create a list called fruits with the names of five different fruits.
+# Add a new fruit to the fruits list, and then remove one of the fruits.
+
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+fruits.append("fig")  # Add a new fruit
+fruits.remove("banana")  # Remove a fruit
+
+print(fruits)
